10f6ef1a85fccc98dd233891d313daac2a3c03d4
pms147 <89722633+pms147@users.noreply.github.com>
2023-12-18 17:03:42 +0700

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+export const Todo = ({task, toggeComplete, editTodo, deleteTodo}) => {
+  return (
+    <div className='Todo'>
+        <p onClick={() => toggeComplete(task.id)}
+        className={`${task.completed ? 'completed':""}`}>{task.task}</p>
+        <div>
+            <FontAwesomeIcon icon={faPenToSquare} onClick={() => editTodo(task.id)}/>
+            <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(task.id)}/>
+        </div>
+    </div>
+  )
+}
